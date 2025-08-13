f083c3177c0e592c438287c9495ac75eec7e7956
jrueter15 <45775156+jrueter15@users.noreply.github.com>
2025-08-13 14:40:16 -0500

    Wrote methods and passed the tests

--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -6,11 +6,7 @@ public class StringUtilities {
     // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
     // characters
     public static boolean shortString(String string){
-        if(string.length() < 5){
-            return true;
-        }else{
-            return false;
-        }
+        return string.length() < 5;
     }
     // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
     public static char firstLetter(String string) {
@@ -19,9 +15,9 @@ public class StringUtilities {
     // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
     // "asparagus" (lowercase only) replaced with 4 stars: "****"
     public static String censorAsparagus(String string){
-        String censoredString = string.replace("asparagus","****");
-        return censoredString;
+        return string.replace("asparagus","****");
     }
+
     // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
     // strings are the same length, return the first string.
     public static String bigger(String string1, String string2){
